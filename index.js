b79975aff36ea28ce5bdb6a091d64525b2424962
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-02-04 13:15:25 +0100

    console warn

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+
+This file is being maintaned by Blockly For Discord. Do NOT Modify in the browser unless you know what you are doing
+
+*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    for (let i = 0; i < 3; i++) {
+        console.log('%cIf someone told you to PASTE something here, do NOT do it. Theres a 101% Chance they are trying to hack your account.', 'color: red; font-size: 16px;');
+      }
+      
+    
+});
