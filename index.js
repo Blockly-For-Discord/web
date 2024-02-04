7e2850ba1475d4c4db04c7ce952d1d2a49ba2c70
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-02-04 13:17:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ This file is being maintaned by Blockly For Discord. Do NOT Modify in the browse
 
 document.addEventListener("DOMContentLoaded", function() {
     for (let i = 0; i < 3; i++) {
+        console.log('%cWAIT!', 'color: white; font-weight: bolder; font-size 30px;')
         console.log('%cIf someone told you to PASTE something here, do NOT do it. Theres a 101% Chance they are trying to hack your account.', 'color: red; font-size: 16px;');
       }
       
