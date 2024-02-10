f0729c805c74c6bd31efa391c72a25cdaaf5581b
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-02-10 15:56:14 +0100

    test

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     BlocklyJS: './webpack/webpack-generator.js',
   },
   output: {
-    library: 'test',
+    library: 'b4d',
     path: path.resolve(__dirname, './components/webpack'),
     filename: '[name].bundle.js', 
   },
--- webpack/webpack-blockly.js
+++ webpack/webpack-blockly.js
@@ -1,3 +1,5 @@
 import * as Blockly from 'blockly/core';
 import * as libraryBlocks from 'blockly/blocks';
 import * as En from 'blockly/msg/en';
+
+b4d.Blockly = Blockly;
