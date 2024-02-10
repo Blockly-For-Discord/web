f844e519b52e56224d4493c234570de94e776653
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-02-10 13:49:45 +0100

    test

--- webpack/webpack-blockly.js
+++ webpack/webpack-blockly.js
@@ -1,3 +1,3 @@
 import * as Blockly from 'blockly/core';
-import * as libraryBlocks from 'blockly/blocks';
-import * as En from 'blockly/msg/en';
+/* import * as libraryBlocks from 'blockly/blocks';
+import * as En from 'blockly/msg/en'; */
