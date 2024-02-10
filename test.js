731ef26ab6fc07a7170eb84343baf6562c240265
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-02-10 11:30:58 +0100

    compressed blockly test

--- test.js
+++ test.js
@@ -1,3 +1,38 @@
 import * as Blockly from './components/webpack/blocklycompressed.bundle.js';
 
+
+const toolbox = {
+    "kind": "flyoutToolbox",
+    "contents": [
+      {
+        "kind": "block",
+        "type": "controls_if"
+      },
+      {
+        "kind": "block",
+        "type": "controls_repeat_ext"
+      },
+      {
+        "kind": "block",
+        "type": "logic_compare"
+      },
+      {
+        "kind": "block",
+        "type": "math_number"
+      },
+      {
+        "kind": "block",
+        "type": "math_arithmetic"
+      },
+      {
+        "kind": "block",
+        "type": "text"
+      },
+      {
+        "kind": "block",
+        "type": "text_print"
+      },
+    ]
+  }
+  
 const workspace = Blockly.inject('blocklyDiv', {toolbox: toolbox});
