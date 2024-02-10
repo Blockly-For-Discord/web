4f4ea120c3e2ee2f140b23ada4bb6f7700da357d
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-02-10 15:31:23 +0100

    Update webpack-blockly.js

--- webpack/webpack-blockly.js
+++ webpack/webpack-blockly.js
@@ -2,4 +2,6 @@ import * as Blockly from 'blockly/core';
 import * as libraryBlocks from 'blockly/blocks';
 import * as En from 'blockly/msg/en';
 
-Blockly = Blockly
+window.libumd = {
+    Blockly,
+  };
