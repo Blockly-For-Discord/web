818fe7c2ea120aeb44a63f143dd541917c20acc8
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-10 15:06:11 +0100

    Update webpack-blockly.js

--- webpack/webpack-blockly.js
+++ webpack/webpack-blockly.js
@@ -1,6 +1,5 @@
 import * as Blockly from 'blockly/core';
-/* import * as libraryBlocks from 'blockly/blocks';
-import * as En from 'blockly/msg/en'; */
+import * as libraryBlocks from 'blockly/blocks';
+import * as En from 'blockly/msg/en';
 
-console.log(Blockly)
 b4d.Blockly = Blockly
