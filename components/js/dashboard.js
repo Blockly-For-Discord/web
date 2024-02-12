bc1c650eb9bbce675c5d8b7a0b4d0bb0879732b8
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-02-12 21:04:04 +0100

    Update dashboard.js

--- components/js/dashboard.js
+++ components/js/dashboard.js
@@ -12,7 +12,7 @@ let b4d = {
   }
 };
 
-document.addEventListener("DOMContentLoaded", async function() {
+document.addEventListener("DOMContentLoaded", function() {
     for (let i = 0; i < 3; i++) {
         console.log('%cWAIT!', 'color: white; font-weight: bolder; font-size 50px;')
         console.log('%cIf someone told you to PASTE something here, do NOT do it. Theres a 101% Chance they are trying to hack your account.', 'color: red; font-size: 16px;');
@@ -21,13 +21,13 @@ document.addEventListener("DOMContentLoaded", async function() {
 
       if (!Cookies.get('token')) {
 
-        ProgressChange(20);
-        const link = document.createElement('a');
-        link.href = 'https://discord.com/api/oauth2/authorize?client_id=1163198844599808254&response_type=code&redirect_uri=https%3A%2F%2Fblockly-for-discord.xyz%2Fcallback&scope=identify+email';
-        link.rel = 'noopener noreferrer';
-        document.body.appendChild(link);
-        await new Promise(resolve => setTimeout(resolve, 0));
-        link.click();
+        ProgressChange(20, function() {
+          const link = document.createElement('a');
+          link.href = 'https://discord.com/api/oauth2/authorize?client_id=1163198844599808254&response_type=code&redirect_uri=https%3A%2F%2Fblockly-for-discord.xyz%2Fcallback&scope=identify+email';
+          link.rel = 'noopener noreferrer';
+          document.body.appendChild(link);
+          link.click();
+      });
 
       } else {
 
@@ -50,9 +50,11 @@ function AuthEvent() {
   }
 }
 
-function ProgressChange(value) {
+function ProgressChange(value, callback) {
   const LoadingBar = document.getElementById('loadbar');
   const Progress = document.getElementById('progress');
   const widthPercentage = LoadingBar.offsetWidth / 100;
     Progress.style.width = widthPercentage * value + 'px';
+
+    callback();
 }
