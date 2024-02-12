e936ef8660a6375cda0db6f482f0cc616bcd7d7e
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-02-12 23:08:49 +0100

    callback test

---
+++ components/js/callback.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    const callback = window.location.search;
+
+    
+    if(!callback === '') {
+
+        var searchParams = new URLSearchParams(callback);
+        var codeValue = searchParams.get('code');
+
+        fetch("https://api.blockly-for-discord.xyz/token", {
+            method: "GET",
+            headers: {
+            "code": codeValue
+            }
+        })
+        .then(data => {
+            if (data.response === "ok") {
+
+            } else {
+
+                window.location.href = "https://blockly-for-discord.xyz?error=request_denied"
+
+            }
+        })
+        .catch(error => {
+            window.location.href = "https://blockly-for-discord.xyz?error=" + data.response
+        })
+            
+        
+        
+
+        window.location.href = "https://blockly-for-discord.xyz/dashboard/";
+    } else {
+        window.location.href = "https://blockly-for-discord.xyz/";
+    }
+});
