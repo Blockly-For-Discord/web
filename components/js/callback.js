109c0aaacce138b76f1be89e63a31ec0e00f1836
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-02-12 23:22:16 +0100

    Update callback.js

--- components/js/callback.js
+++ components/js/callback.js
@@ -1,39 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
     const callback = window.location.search;
 
-    
-    if(callback !== '') {
-
+    if (callback !== '') {
         var searchParams = new URLSearchParams(callback);
         var codeValue = searchParams.get('code');
 
         fetch("https://api.blockly-for-discord.xyz/token", {
             method: "GET",
             headers: {
-            "code": codeValue
+                "code": codeValue
             }
+        })
+        .then(response => {
+            if (!response.ok) {
+                window.location.href = "https://blockly-for-discord.xyz?error=bad_response";
+            }
+            return response.json();
         })
         .then(data => {
             if (data.response === "ok") {
                 const tokenHeaderValue = response.headers.get('token');
                 Cookies.set('token', tokenHeaderValue, { expires: 7 });
                 window.location.href = "https://blockly-for-discord.xyz/dashboard/";
-
             } else {
-
-                window.location.href = "https://blockly-for-discord.xyz?error=request_denied"
-
+                window.location.href = "https://blockly-for-discord.xyz?error=request_denied";
             }
         })
         .catch(error => {
-            window.location.href = "https://blockly-for-discord.xyz?error=" + data.response
-        })
-            
-        
-        
-
-        
+            console.error('Error during fetch:', error);
+            window.location.href = "https://blockly-for-discord.xyz?error=generic_error";
+        });
     } else {
         window.location.href = "https://blockly-for-discord.xyz/";
     }
