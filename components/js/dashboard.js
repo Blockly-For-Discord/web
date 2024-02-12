f8a77acba98c036bf425581ad4563bb5a224dbdd
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-02-12 20:58:14 +0100

    Update dashboard.js

--- components/js/dashboard.js
+++ components/js/dashboard.js
@@ -12,7 +12,7 @@ let b4d = {
   }
 };
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", async function() {
     for (let i = 0; i < 3; i++) {
         console.log('%cWAIT!', 'color: white; font-weight: bolder; font-size 50px;')
         console.log('%cIf someone told you to PASTE something here, do NOT do it. Theres a 101% Chance they are trying to hack your account.', 'color: red; font-size: 16px;');
@@ -20,15 +20,13 @@ document.addEventListener("DOMContentLoaded", function() {
       
 
       if (!Cookies.get('token')) {
-        const LoadingBar = document.getElementById('loadbar');
-        const Progress = document.getElementById('progress');
-        const widthPercentage = LoadingBar.offsetWidth / 100;
-          Progress.style.width = widthPercentage * 20 + 'px';
-          
+
+        ProgressChange(20);
         const link = document.createElement('a');
         link.href = 'https://discord.com/api/oauth2/authorize?client_id=1163198844599808254&response_type=code&redirect_uri=https%3A%2F%2Fblockly-for-discord.xyz%2Fcallback&scope=identify+email';
         link.rel = 'noopener noreferrer';
         document.body.appendChild(link);
+        await new Promise(resolve => setTimeout(resolve, 0));
         link.click();
 
       } else {
@@ -50,4 +48,11 @@ function AuthEvent() {
     document.dispatchEvent(authEvent);
     
   }
+}
+
+function ProgressChange(value) {
+  const LoadingBar = document.getElementById('loadbar');
+  const Progress = document.getElementById('progress');
+  const widthPercentage = LoadingBar.offsetWidth / 100;
+    Progress.style.width = widthPercentage * value + 'px';
 }
