dfc21e72d2a52e01bd051b524de8d1285a2caea1
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-02-13 12:26:06 +0100

    Update index.js

--- components/js/index.js
+++ components/js/index.js
@@ -4,10 +4,25 @@ document.addEventListener('DOMContentLoaded', function() {
     const callback = window.location.search;
     if (callback !== '') {
         var searchParams = new URLSearchParams(callback);
-        var codeValue = searchParams.get('error');
+        var ErrorMessage = searchParams.get('error');
 
-        ThrowErrorMessage(codeValue);
+        ThrowErrorMessage(ErrorMessage);
     }
 })
 
 
+function ThrowErrorMessage (text) {
+    b4d.Toastify({
+        text: "An Error occurred while logging you in: " + text,
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "bottom",
+        position: "center", 
+        stopOnFocus: false,
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){}
+      }).showToast();
+}
