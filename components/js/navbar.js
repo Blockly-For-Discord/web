dd77de5b54cde31220abcf5e851791e1e72bd0c3
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-02-22 16:14:22 +0100

    Update navbar.js

--- components/js/navbar.js
+++ components/js/navbar.js
@@ -3,7 +3,7 @@
 function SetNavbar (html) {
     var nav = document.getElementById('navbar');
 
-    if (element !== null) {
+    if (nav !== null) {
         nav.innerHTML = html;
     } else {
         console.log("[B4D] Navbar does not exist")
