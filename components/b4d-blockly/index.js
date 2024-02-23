c37bd170b60d4b41fba5956e6a0f1e07037b4ebb
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-02-23 23:50:45 +0100

    lib conf

---
+++ components/b4d-blockly/index.js
@@ -0,0 +1,1 @@
+const libs = ["discordjs", "discordpy"]
---
+++ components/b4d-blockly/libraries/discordjs/index.js
@@ -0,0 +1,9 @@
+
+import discordjs_tree from './file-tree.js';
+
+const LibInfo = {
+    "name":"DiscordJS",
+    "id":"discordjs",
+    "version":"v0.0.0",
+    "description": "discord.js is a powerful Node.js module that allows you to interact with the Discord API very easily.",
+}
