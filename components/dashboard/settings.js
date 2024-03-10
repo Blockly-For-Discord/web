98df9ad675d1367852bba4ee219e04439b9f443b
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-03-10 13:29:13 +0100

    settings

--- components/dashboard/settings.js
+++ components/dashboard/settings.js
@@ -5,9 +5,25 @@ export function SettingsInit (query) {
     window.showLoading(new Promise(r => setTimeout(r, 1500)));
     console.log("Settings Page Loaded");
     const container = document.getElementById("content");
-    container.innerHTML = "Settings Site should load here";
     history.pushState({}, "", "/dashboard/settings");
     document.title = "B4D - Settings";
+
+    const box = document.createElement("div");
+    container.appendChild(box);
+    box.classList.add("settings-container");
+
+    // create sidebar
+    const sidebar = document.createElement("div");
+    sidebar.classList.add("settings-sidebar");
+
+    // create main box
+    const main = document.createElement("div");
+    main.classList.add("settings-box");
+
+    // append as children
+
+    box.appendChild(sidebar);
+    box.appendChild(main);
 }
 
 export async function SettingsSwitch () {
