97da627e5735f8336f36f105676e102b7e5b2d1f
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-04-21 20:40:39 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,16 +3,12 @@ import * as Blockly from './components/webpack/blocklycompressed.bundle.js';
 import { DiscordJStooblox } from './components/b4d-blockly/libraries/discordjs/index.js';
 import * as DiscordJS from './components/b4d-blockly/libraries/discordjs/blocks.js';
 
-
-
 const workspace = b4d.Blockly.inject('blocklyDiv', {
   toolbox: DiscordJStooblox,
   renderer: 'zelos'
 });
 
-b4d.toolbox = toolbox;
+b4d.toolbox = DiscordJStooblox;
 b4d.workspace = workspace;
 
 b4d.Blockly.setLocale(b4d.En);
-
-
