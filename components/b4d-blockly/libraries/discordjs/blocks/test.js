78a68c121d834737c2e0fd1a160527ddce2a178f
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-05-11 12:08:59 +0200

    Fix maybe

--- components/b4d-blockly/libraries/discordjs/blocks/test.js
+++ components/b4d-blockly/libraries/discordjs/blocks/test.js
@@ -1,5 +1,4 @@
-
-import * as Blockly from './components/webpack/blocklycompressed.bundle.js';
+import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
 
 export const ExampleBlock = "aw_block";
 
