b36e2eb0ddabc589361df8aa302572174e1d4bd4
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-05-14 18:58:54 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import * as Blockly from './components/webpack/blocklycompressed.bundle.js';
 // import './components/b4d-blockly/libraries/discordjs/blocks/test.js';
 import { DiscordJStooblox } from './b4d-blockly/libraries/discordjs/index.js';
 import * as DiscordJS from './b4d-blockly/libraries/discordjs/blocks.js';
-
+import { Block } from '/b4d-blockly/index.js';
 
 
 
