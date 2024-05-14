d33e1177fc27f216e468492a7abef4d9fac7bd26
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-05-14 22:16:15 +0200

    Update index.js

--- b4d-blockly/index.js
+++ b4d-blockly/index.js
@@ -1,4 +1,10 @@
 export class Block {
+    
+    constructor(namespace, block_name, data) {
+        this.namespace = namespace;
+        this.block_name = block_name;
+        this.data = data;
+    }
 
     static namespaces = [];
     static blocks = {};
@@ -21,14 +27,6 @@ export class Block {
         }
     }
 
-    
-
-    constructor(namespace, block_name, data) {
-        this.namespace = namespace;
-        this.block_name = block_name;
-        this.data = data;
-    }
-
     attach() {
         if (!Block.blocks[this.namespace]) Block.blocks[this.namespace] = {};
         Block.blocks[this.namespace][this.block_name] = this.data;
@@ -37,8 +35,27 @@ export class Block {
             Block.namespaces.push(this.namespace);
         }
 
+    }
+
+    dispose() {
+        b4d.workspace.getBlockById(this.namespace + ":" + this.block_name).dispose();
+    }
+}
+
+export class Extension {
+    constructor(namespace, blocks) {
         
     }
 
+    Attach() {
 
+    }
+    static Register() {
+
+    }
+
+    static Dispose() {
+
+    }
 }
+
