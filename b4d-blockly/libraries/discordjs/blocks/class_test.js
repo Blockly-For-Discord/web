2187b04c37e1d3045402562699ac936ff8111bfc
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-05-14 19:30:43 +0200

    Update class_test.js

--- b4d-blockly/libraries/discordjs/blocks/class_test.js
+++ b4d-blockly/libraries/discordjs/blocks/class_test.js
@@ -1,10 +1,4 @@
-import * as Blockly from 'blockly/core';
-// Import the default blocks.
-import * as libraryBlocks from 'blockly/blocks';
-// Import a generator.
-import {javascriptGenerator} from 'blockly/javascript';
-// Import a message file.
-import * as En from 'blockly/msg/en';
+
 import { Block } from '/b4d-blockly/index.js';
 
 const MyBlock = new Block('discordjs', 'aw_block', {
@@ -24,7 +18,7 @@ const MyBlock = new Block('discordjs', 'aw_block', {
 MyBlock.attach();
 
 
-javascriptGenerator.forBlock['discordjs:aw_block'] = function(block, generator) {
+b4d.javascriptGenerator.forBlock['discordjs:aw_block'] = function(block, generator) {
     var value_name = generator.valueToCode(block, 'STRING', javascript.Order.ATOMIC);
     var code = `awwwwwwwww ${value_name}`;
     return code;
