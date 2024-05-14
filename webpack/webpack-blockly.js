9cead9c5c4abc6dd770355a8b0fdbf42d25c5a7a
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-05-14 19:41:38 +0200

    Update webpack-blockly.js

--- webpack/webpack-blockly.js
+++ webpack/webpack-blockly.js
@@ -4,6 +4,7 @@ import * as En from 'blockly/msg/en';
 import {javascriptGenerator} from 'blockly/javascript';
 
 b4d.Blockly = Blockly
+Blockly = Blockly
 b4d.libraryBlocks = libraryBlocks
 b4d.En = En
 b4d.javascriptGenerator = javascriptGenerator
