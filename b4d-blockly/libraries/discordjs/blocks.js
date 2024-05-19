31602158eb3eb1ace5a36f3e7a86635e7744c50e
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-05-19 11:10:27 +0200

    Update blocks.js

--- b4d-blockly/libraries/discordjs/blocks.js
+++ b4d-blockly/libraries/discordjs/blocks.js
@@ -1,3 +1,8 @@
+// Exports
+// Test
 export * from '/b4d-blockly/libraries/discordjs/blocks/class_test.js';
+
+// Base
 export * from '/b4d-blockly/libraries/discordjs/blocks/base/login.js';
 export * from '/b4d-blockly/libraries/discordjs/blocks/base/login_advanced.js';
+export * from '/b4d-blockly/libraries/discordjs/blocks/base/env.js';
