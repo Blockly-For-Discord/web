ba933e3d605d7c3a77d58053e7508141b6c5828d
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-05-19 10:03:37 +0200

    Create login_advanced.js

---
+++ b4d-blockly/libraries/discordjs/blocks/base/login_advanced.js
@@ -0,0 +1,47 @@
+import { Block } from '/b4d-blockly/index.js';
+import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
+
+const base_login_advanced = new Block('discordjs', 'base_login_advanced', {
+    "message0": "Start bot with token %1 Listen to Events %2 Mobile? %3",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "token",
+        "check": "String"
+      },
+      {
+        "type": "input_value",
+        "name": "intents",
+        "check": "Array"
+      },
+      {
+        "type": "field_checkbox",
+        "name": "mobile",
+        "checked": false
+      }
+    ],
+    "colour": "#009dff",
+    "tooltip": "Start your bot with more control",
+    "helpUrl": ""
+});
+
+base_login_advanced.attach();
+
+
+b4d.javascriptGenerator.forBlock['discordjs:base_login_advanced'] = function(block, generator) {
+  var value_token = generator.valueToCode(block, 'token', b4d.javascriptGenerator.ORDER_ATOMIC);
+  var value_intents = generator.valueToCode(block, 'intents', javascript.Order.ATOMIC);
+  var value_mobile = block.getFieldValue('mobile') === 'TRUE';
+  var code = `const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: ${value_intents}${value_mobile ? ',\n  ws: { properties: { browser: "Discord iOS" }}':''}
+});
+
+client.once(Events.ClientReady, c => {
+  console.log(\`Ready! Logged in as \${c.user.tag} \`);
+});
+
+client.login(${value_token});\n`;
+    return code;
+};
