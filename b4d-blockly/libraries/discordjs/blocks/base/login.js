259f0fd8b161ddca4fd9ddd64ae3c07c37ddda8f
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-05-20 17:12:06 +0200

    Update login.js

--- b4d-blockly/libraries/discordjs/blocks/base/login.js
+++ b4d-blockly/libraries/discordjs/blocks/base/login.js
@@ -2,25 +2,24 @@ import { Block } from '/b4d-blockly/index.js';
 import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
 
 const base_login = new Block('discordjs', 'base_login', {
-    "message0": "Start bot with token %1",
-    "args0": [
-      {
-        "type": "input_value",
-        "name": "token",
-        "check": "String"
-      }
-    ],
-    "colour": "#009dff",
-    "tooltip": "Start your discord bot with a token",
-    "helpUrl": ""
+  "message0": "Start bot with token %1",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "token",
+      "check": "String"
+    }
+  ],
+  "colour": "#009dff",
+  "tooltip": "Start your discord bot with a token",
+  "helpUrl": ""
 });
 
 base_login.attach();
 
-
 b4d.javascriptGenerator.forBlock['discordjs:base_login'] = function(block, generator) {
-    var value_token = generator.valueToCode(block, 'token', b4d.javascriptGenerator.ORDER_ATOMIC);
-    var code = `const { Client, Events, GatewayIntentBits, ActivityType } = require('discord.js');
+  var value_token = generator.valueToCode(block, 'token', b4d.javascriptGenerator.ORDER_ATOMIC);
+  var code = `const { Client, Events, GatewayIntentBits, ActivityType } = require('discord.js');
 
 const client = new Client({
   intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildPresences ]
@@ -31,5 +30,5 @@ client.once(Events.ClientReady, c => {
 });
 
 client.login(${value_token});\n`;
-    return code;
+  return code;
 };
