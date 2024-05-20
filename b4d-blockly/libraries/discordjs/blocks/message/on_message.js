350b44bcf23ac054a887f1641a1d019013fbb405
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-05-20 16:51:58 +0200

    Create on_message.js

---
+++ b4d-blockly/libraries/discordjs/blocks/message/on_message.js
@@ -0,0 +1,28 @@
+import { Block } from '/b4d-blockly/index.js';
+import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
+
+const message_on_message = new Block('discordjs', 'message_on_message', {
+  "message0": "When message received %1 %2",
+  "args0": [
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_statement",
+      "name": "in"
+    }
+  ],
+  "colour": "#0040ff",
+  "tooltip": "Do something when a message is received",
+  "helpUrl": ""
+});
+
+message_on_message.attach();
+
+b4d.javascriptGenerator.forBlock['discordjs:message_on_message'] = function(block, generator) {
+  var statement_in = generator.statementToCode(block, 'in');
+  var code = `client.on(Events.MessageCreate, async(message) => {
+  ${statement_in}
+});\n`;
+  return code;
+};
