0b5c5be8c4e3c853ce271b62b4434a5d4b307b72
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-06-22 11:29:33 +0200

    test

--- components/js/version.js
+++ components/js/version.js
@@ -1,4 +1,17 @@
-import branch from "/branch.json" with { type: "json" };
+fetch('/branch.json')
+ .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+ .then(data => {
+    const branch = data;
+  })
+ .catch(error => {
+    console.error('[Error] An error ocurred fetching branch information. Error: ' + error);
+  });
+
 
 
 let b4d = {
