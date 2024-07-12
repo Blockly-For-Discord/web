6ca18398d0af1191220cfafacc437266f908e505
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-07-12 22:44:45 +0200

    Update login.js

--- b4d-blockly/libraries/discordjs/blocks/base/login.js
+++ b4d-blockly/libraries/discordjs/blocks/base/login.js
@@ -19,7 +19,7 @@ base_login.attach();
 
 b4d.javascriptGenerator.forBlock['discordjs:base_login'] = function(block, generator) {
   var value_token = generator.valueToCode(block, 'token', b4d.javascriptGenerator.ORDER_ATOMIC);
-  var code = `const { Client, Events, GatewayIntentBits, ActivityType } = require('discord.js');
+  var code = `const { ActivityType, Client, Events, EmbedBuilder, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
   intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildPresences ]
