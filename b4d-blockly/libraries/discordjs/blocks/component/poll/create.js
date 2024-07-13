6d76f8492ab7c896408bc58dc46ab63c05e326aa
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-07-13 16:10:52 +0200

    Update create.js

--- b4d-blockly/libraries/discordjs/blocks/component/poll/create.js
+++ b4d-blockly/libraries/discordjs/blocks/component/poll/create.js
@@ -2,11 +2,16 @@ import { Block } from '/b4d-blockly/index.js';
 import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
 
 const poll_create = new Block('discordjs', 'poll_create', {
-  "message0": "Create poll with name %1 duration (hours) %2 multi select %3 with answers %4 %5",
+  "message0": "Create poll with name %1 question %2 duration (hours) %3 multi select %4 with answers %5 %6",
   "args0": [
     {
       "type": "input_value",
       "name": "name",
+      "check": "String"
+    },
+    {
+      "type": "input_value",
+      "name": "question",
       "check": "String"
     },
     {
@@ -39,11 +44,12 @@ poll_create.attach();
 
 b4d.javascriptGenerator.forBlock['discordjs:poll_create'] = function(block, generator) {
   var value_name = generator.valueToCode(block, 'name', b4d.javascriptGenerator.ORDER_ATOMIC);
+  var value_question = generator.valueToCode(block, 'question', b4d.javascriptGenerator.ORDER_ATOMIC);
   var value_time = generator.valueToCode(block, 'time', b4d.javascriptGenerator.ORDER_ATOMIC);
   var value_multi = generator.valueToCode(block, 'multi', b4d.javascriptGenerator.ORDER_ATOMIC);
   var statement_answers = generator.statementToCode(block, 'answers');
   var code = `var poll_${value_name.replaceAll(' ','_').replaceAll('-','_').replaceAll("'",'')} = {
-  question: { text: ${value_name} },
+  question: { text: ${value_question} },
   duration: ${value_time},
   allow_multiselect: ${value_multi},
   answers: [
