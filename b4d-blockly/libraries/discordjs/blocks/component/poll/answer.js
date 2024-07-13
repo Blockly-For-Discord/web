3af7f4c5ba504c00f7a9f4936111164d2e46f471
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-07-13 14:42:44 +0200

    Create answer.js

---
+++ b4d-blockly/libraries/discordjs/blocks/component/poll/answer.js
@@ -0,0 +1,35 @@
+import { Block } from '/b4d-blockly/index.js';
+import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
+
+const poll_answer = new Block('discordjs', 'poll_answer', {
+  "message0": "Answer with text %1 emoji %2",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "text",
+      "check": "String"
+    },
+    {
+      "type": "input_value",
+      "name": "emoji",
+      "check": "String"
+    }
+  ],
+  "inputsInline": false,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": "#bf70e3",
+  "tooltip": "Add an answer to a poll (emoji is optional)",
+  "helpUrl": ""
+});
+
+poll_answer.attach();
+
+b4d.javascriptGenerator.forBlock['discordjs:poll_create'] = function(block, generator) {
+  var value_text = generator.valueToCode(block, 'text', b4d.javascriptGenerator.ORDER_ATOMIC);
+  var value_emoji = generator.valueToCode(block, 'emoji', b4d.javascriptGenerator.ORDER_ATOMIC);
+  var code = `{
+  text: ${value_text}${value_emoji.length > 2 ? `,\n  emoji: ${value_emoji}` : ''}
+},\n`;
+  return code;
+};
