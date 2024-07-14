3bb0fa1ce6b8f005ab212c37438fe6feee2b0b0a
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-07-14 18:35:03 +0200

    Update dashboard.js

--- components/js/dashboard.js
+++ components/js/dashboard.js
@@ -57,182 +57,18 @@ document.addEventListener("DOMContentLoaded", async function() {
       }
       
 
-
+    // IMPORTANT: ENTRY POINT
       if (Cookies.get('access_token') && Cookies.get('refresh_token')) {
-        // if user has token. type: access_token
-        const pt = document.getElementById('LoadScreenText');
-        const ls = document.getElementById('loadscreen');
-        const fadeInAnimation = {
-          targets: ls,
-          opacity: [1, 0],
-          duration: 1200,
-          easing: 'easeInOutSine'
-      };
-
-        
-        ProgressChange(20);
-        // verified that server is online
-        pt.textContent = 'Fetching user data...';
-        // get user data (profile, username)
-        ;
-          AuthEvent()
-        setTimeout(function() {
-          ProgressChange(100);
-          setTimeout( function() {
-            anime(fadeInAnimation);
-            setTimeout( function() {
-              ls.style.display = 'none';
-            }, 1200);
-              
-          }, 1000);
-          
-        }, 2500);
-
-        
-
+ 
       } else if (Cookies.get('refresh_token')) {
         // if user has refresh_token but no token. type: refresh_token
         
       } else {
         // if user has no token or refresh token. type: code_grant
-        ProgressChange(20);
-        const pt = document.getElementById('LoadScreenText');
-        setTimeout(function() {
-          const link = document.createElement('a');
-          link.href = b4d.branch("oauth.redirect_uri");
-          link.rel = 'noopener noreferrer';
-          document.body.appendChild(link);
-          pt.textContent = 'Redirecting...';
-          link.click();
-        }, 1400);
+ 
       }
     
 
 });
 
 
-
-
-let AuthEventHasRun = false;
-
-function AuthEvent() {
-  if (!AuthEventHasRun) {
-    hasRun = AuthEventHasRun;
-    const authEvent = new CustomEvent("AuthConfirmed");
-    document.dispatchEvent(authEvent);
-    
-  }
-}
-
-function ProgressChange (value) {
-  const LoadingBar = document.getElementById('loadbar');
-  const Progress = document.getElementById('progress');
-  const widthPercentage = LoadingBar.offsetWidth / 100;
-  Progress.style.width = widthPercentage * value + 'px';
-
-    
-}
-
-
-function TippyJS (router) {
-  b4d.tippy('#avatar-container', {
-
-    content: "I'm a Tippy tooltip!",
-    placement: 'right',
-    animation:'perspective',
-    allowHTML: true,
-    interactive: true,
-    theme: 'b4ddark',
-    arrow: false,
-  });
-  // sidebar tooltips
-  for (let key in router) {
-    if (router.hasOwnProperty(key)) {
-        const entry = router[key];
-        if (entry.tooltip) {
-            const icon = entry.icon;
-            const tooltip = entry.tooltip;
-
-            console.log("Icon:", icon, "Tooltip:", tooltip);
-            b4d.tippy('#' + icon, {
-
-              content: tooltip,
-              placement: 'right',
-              animation:'perspective',
-              theme: 'b4ddark',
-              arrow: false,
-            });
-        }
-    }
-}
-}
-
-window.TippyJS = TippyJS;
-
-function DeployWindow(options) {
-
-  const {
-    title = "Default Title",
-    content = `<p>Default content</p>`,
-    closable = false,
-    width,
-    height,
-    showInfoIcon = false,
-    info = "",
-    showIcon = false,
-    icon = "",
-    buttons = '',
-    callback = null,
-    animationOnAppear = "",
-    animationOnDissapear = "",
-    animationDuration = 0,
-    duration = 0,
-    id,
-  } = options;
-
-  const preset = `
-  <div id="WindowContainer">
-  <div id="WindowTop">
-      <div id="WindowTitleBar">
-          <div id="WindowTitle"></div>
-          <div id="CloseButton"></div>
-      </div>
-      <div id="WindowContent"></div>
-  </div>
-  <div id="WindowBottom">
-      <div id="info-section">
-          <div id="info-icon"></div>
-          <div id="info-text"></div>
-      </div>
-      <div id="button-section"></div>
-  </div>
-</div>
-  `;
-
-  const PContainer = document.createElement('div');
-  PContainer.classList.add("ParentContainerCover");
-  PContainer.innerHTML = preset;
-  PContainer.getElementById("WindowTitle").innerHTML = title;
-  PContainer.getElementById("WindowContent").innerHTML = content;
-  //closable
-  if (!closable) {
-    PContainer.getElementById("CloseButton").style.display = "none";
-  } else {
-    PContainer.getElementById("CloseButton").addEventListener('click', function() {
-      // CloseWindow method goes here
-    });
-  }
-
-  // width, height
-  PContainer.getElementById("WindowContainer").style.width = width;
-  PContainer.getElementById("WindowContainer").style.height = height;
-
-  // Show Info icon
-  if (!showInfoIcon) {
-    PContainer.getElementById("info-icon").style.display = "none";
-  }
-};
-
-function DeployWindowExists () {
-
-}
