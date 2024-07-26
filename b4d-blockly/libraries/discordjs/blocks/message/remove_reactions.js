e3e17924a45807ba3196715e2a763ab6ad1a8a9f
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-07-26 15:09:58 +0200

    Create remove_reactions.js

---
+++ b4d-blockly/libraries/discordjs/blocks/message/remove_reactions.js
@@ -0,0 +1,27 @@
+import { Block } from '/b4d-blockly/index.js';
+import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
+
+const delete_reactions = new Block('discordjs', 'delete_reactions', {
+  "message0": "Remove all reactions on %1",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "message",
+      "check": "Message"
+    }
+  ],
+  "inputsInline": false,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": "#05b55a",
+  "tooltip": "Remove all reactions in a message",
+  "helpUrl": ""
+});
+
+delete_reactions.attach();
+
+b4d.javascriptGenerator.forBlock['discordjs:delete_reactions'] = function(block, generator) {
+  var value_message = generator.valueToCode(block, 'message', b4d.javascriptGenerator.ORDER_ATOMIC);
+  var code = `${value_message}.reactions.removeAll();\n`;
+  return code;
+};
