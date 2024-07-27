35dc59bb3690a91a699289ec6b298059d801e63c
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-07-27 18:05:59 +0200

    Create component.js

---
+++ b4d-blockly/libraries/discordjs/blocks/component/component.js
@@ -0,0 +1,30 @@
+import { Block } from '/b4d-blockly/index.js';
+import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
+
+const component = new Block('discordjs', 'component', {
+  "message0": "Create component with %1 poll %2",
+  "args0": [
+    {
+      "type": "component"
+    },
+    {
+      "type": "input_value",
+      "name": "poll",
+      "check": "String"
+    }
+  ],
+  "inputsInline": false,
+  "previousStatement": "Answers",
+  "nextStatement": "Answers",
+  "colour": b4d.color.component,
+  "tooltip": "Create a component (All inputs optional)",
+  "helpUrl": ""
+});
+
+component.attach();
+
+b4d.javascriptGenerator.forBlock['discordjs:component'] = function(block, generator) {
+  var value_poll = generator.valueToCode(block, 'poll', b4d.javascriptGenerator.ORDER_ATOMIC);
+  var code = `${value_poll.length > 2 ? `poll: poll_${value_poll.replaceAll(' ','_').replaceAll('-','_').replaceAll("'",'')}` : ''}\n`;
+  return code;
+};
