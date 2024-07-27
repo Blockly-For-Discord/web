a88d90a7c6e251d044ec06876b2885c5fcac1c9d
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-07-27 12:21:22 +0200

    Update react.js

--- b4d-blockly/libraries/discordjs/blocks/message/react.js
+++ b4d-blockly/libraries/discordjs/blocks/message/react.js
@@ -2,15 +2,20 @@ import { Block } from '/b4d-blockly/index.js';
 import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
 
 const message_react = new Block('discordjs', 'message_react', {
-  "message0": "React with %1",
+  "message0": "React with %1 to %2",
   "args0": [
     {
       "type": "input_value",
       "name": "emoji",
       "check": "String"
+    },
+    {
+      "type": "input_value",
+      "name": "message",
+      "check": "Message"
     }
   ],
-  "inputsInline": false,
+  "inputsInline": true,
   "previousStatement": null,
   "nextStatement": null,
   "colour": "#05b55a",
@@ -22,6 +27,7 @@ message_react.attach();
 
 b4d.javascriptGenerator.forBlock['discordjs:message_react'] = function(block, generator) {
   var value_emoji = generator.valueToCode(block, 'emoji', b4d.javascriptGenerator.ORDER_ATOMIC);
-  var code = `message.react(${value_emoji});\n`;
+  var value_message = generator.valueToCode(block, 'message', b4d.javascriptGenerator.ORDER_ATOMIC);
+  var code = `${value_message}.react(${value_emoji});\n`;
   return code;
 };
