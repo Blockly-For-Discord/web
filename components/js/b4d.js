66e7e387ea67700aae1fb2c695873fdaa8e97eae
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-07-27 16:16:15 +0200

    Update b4d.js

--- components/js/b4d.js
+++ components/js/b4d.js
@@ -6,6 +6,8 @@ This file is being maintaned by Blockly For Discord. Do NOT Modify in the browse
 
 let b4d = {
   color: {
-    console: '#8D5BCA'
+    console: '#8D5BCA',
+    base: '#009dff',
+    message: '#05b55a'
   }
 };
