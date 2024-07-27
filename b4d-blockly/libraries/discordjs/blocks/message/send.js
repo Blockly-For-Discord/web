bebb8cb96263bed0a348093a83623b787d2a3231
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-07-27 18:36:40 +0200

    Update send.js

--- b4d-blockly/libraries/discordjs/blocks/message/send.js
+++ b4d-blockly/libraries/discordjs/blocks/message/send.js
@@ -2,7 +2,7 @@ import { Block } from '/b4d-blockly/index.js';
 import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
 
 const message_send = new Block('discordjs', 'message_send', {
-  "message0": "Send %1 in %2",
+  "message0": "Send %1 component %2 in %3",
   "args0": [
     {
       "type": "input_value",
@@ -11,11 +11,16 @@ const message_send = new Block('discordjs', 'message_send', {
     },
     {
       "type": "input_value",
+      "name": "component",
+      "check": "Component"
+    },
+    {
+      "type": "input_value",
       "name": "channel",
       "check": "Channel"
     }
   ],
-  "inputsInline": true,
+  "inputsInline": false,
   "previousStatement": null,
   "nextStatement": null,
   "colour": b4d.color.message,
@@ -27,9 +32,11 @@ message_send.attach();
 
 b4d.javascriptGenerator.forBlock['discordjs:message_send'] = function(block, generator) {
   var value_text = generator.valueToCode(block, 'text', b4d.javascriptGenerator.ORDER_ATOMIC);
+  var value_component = generator.valueToCode(block, 'component', b4d.javascriptGenerator.ORDER_ATOMIC);
   var value_channel = generator.valueToCode(block, 'channel', b4d.javascriptGenerator.ORDER_ATOMIC);
   var code = `${value_channel}.send({
-  content: ${value_text}
+  content: ${value_text},
+  ${value_component}
 });\n`;
   return code;
 };
