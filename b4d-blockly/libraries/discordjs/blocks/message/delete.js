431449be6c16a0e8d5d20c32120ab0158d2b00f7
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-07-27 12:06:52 +0200

    Update delete.js

--- b4d-blockly/libraries/discordjs/blocks/message/delete.js
+++ b4d-blockly/libraries/discordjs/blocks/message/delete.js
@@ -2,7 +2,14 @@ import { Block } from '/b4d-blockly/index.js';
 import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
 
 const message_delete = new Block('discordjs', 'message_delete', {
-  "message0": "Delete",
+  "message0": "Delete message %1",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "message",
+      "check": "Message"
+    }
+  ],
   "previousStatement": null,
   "nextStatement": null,
   "colour": "#05b55a",
@@ -13,6 +20,7 @@ const message_delete = new Block('discordjs', 'message_delete', {
 message_delete.attach();
 
 b4d.javascriptGenerator.forBlock['discordjs:message_delete'] = function(block, generator) {
-  var code = `message.delete();\n`;
+  var value_message = generator.valueToCode(block, 'message', b4d.javascriptGenerator.ORDER_ATOMIC);
+  var code = `${value_message}.delete();\n`;
   return code;
 };
