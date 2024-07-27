cffe4bb38b5e961ea358cfb7f48269e020ef51b7
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-07-27 12:16:20 +0200

    Update remove_reaction.js

--- b4d-blockly/libraries/discordjs/blocks/message/remove_reaction.js
+++ b4d-blockly/libraries/discordjs/blocks/message/remove_reaction.js
@@ -1,7 +1,7 @@
 import { Block } from '/b4d-blockly/index.js';
 import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
 
-const remove_reaction = new Block('discordjs', 'remove_reaction', {
+const message_remove_reaction = new Block('discordjs', 'message_remove_reaction', {
   "message0": "Remove %1 reactions from %2",
   "args0": [
     {
@@ -23,9 +23,9 @@ const remove_reaction = new Block('discordjs', 'remove_reaction', {
   "helpUrl": ""
 });
 
-remove_reaction.attach();
+message_remove_reaction.attach();
 
-b4d.javascriptGenerator.forBlock['discordjs:remove_reaction'] = function(block, generator) {
+b4d.javascriptGenerator.forBlock['discordjs:message_remove_reaction'] = function(block, generator) {
   var value_emoji = generator.valueToCode(block, 'emoji', b4d.javascriptGenerator.ORDER_ATOMIC);
   var value_message = generator.valueToCode(block, 'message', b4d.javascriptGenerator.ORDER_ATOMIC);
   var code = `${value_message}.reactions.cache.get(${value_emoji}).remove();\n`;
