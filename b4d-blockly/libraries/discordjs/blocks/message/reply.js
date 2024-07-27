b7c42dacfaddac86d144b9d1deb2e80cd5b67d6f
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-07-27 12:20:39 +0200

    Update reply.js

--- b4d-blockly/libraries/discordjs/blocks/message/reply.js
+++ b4d-blockly/libraries/discordjs/blocks/message/reply.js
@@ -2,7 +2,7 @@ import { Block } from '/b4d-blockly/index.js';
 import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
 
 const message_reply = new Block('discordjs', 'message_reply', {
-  "message0": "Reply %1 Mention %2",
+  "message0": "Reply %1 Mention %2 to %3",
   "args0": [
     {
       "type": "input_value",
@@ -13,6 +13,11 @@ const message_reply = new Block('discordjs', 'message_reply', {
       "type": "input_value",
       "name": "mention",
       "check": "Boolean"
+    },
+    {
+      "type": "input_value",
+      "name": "message",
+      "check": "Message"
     }
   ],
   "inputsInline": false,
@@ -28,6 +33,7 @@ message_reply.attach();
 b4d.javascriptGenerator.forBlock['discordjs:message_reply'] = function(block, generator) {
   var value_text = generator.valueToCode(block, 'text', b4d.javascriptGenerator.ORDER_ATOMIC);
   var value_mention = generator.valueToCode(block, 'mention', b4d.javascriptGenerator.ORDER_ATOMIC);
-  var code = `message.reply({ content: ${value_text}, allowedMentions: { repliedUser: ${value_mention} }});\n`;
+  var value_message = generator.valueToCode(block, 'message', b4d.javascriptGenerator.ORDER_ATOMIC);
+  var code = `${value_message}.reply({ content: ${value_text}, allowedMentions: { repliedUser: ${value_mention} }});\n`;
   return code;
 };
