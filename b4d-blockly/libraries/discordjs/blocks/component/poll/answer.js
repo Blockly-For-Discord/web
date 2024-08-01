28fc7ae535d7aa379faf33d4466bd13b3d275c29
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-08-01 14:07:56 +0200

    Update answer.js

--- b4d-blockly/libraries/discordjs/blocks/component/poll/answer.js
+++ b4d-blockly/libraries/discordjs/blocks/component/poll/answer.js
@@ -1,7 +1,7 @@
-import { Block } from '/b4d-blockly/index.js';
+import { discordjs } from '/b4d-blockly/libraries/discordjs.js';
 import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
 
-const poll_answer = new Block('discordjs', 'poll_answer', {
+discordjs.createBlock('poll_answer', {
   "message0": "Answer with text %1 emoji %2",
   "args0": [
     {
@@ -22,8 +22,6 @@ const poll_answer = new Block('discordjs', 'poll_answer', {
   "tooltip": "Add an answer to a poll (emoji is optional)",
   "helpUrl": ""
 });
-
-poll_answer.attach();
 
 b4d.javascriptGenerator.forBlock['discordjs:poll_answer'] = function(block, generator) {
   var value_text = generator.valueToCode(block, 'text', b4d.javascriptGenerator.ORDER_ATOMIC);
