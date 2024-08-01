f9af764a85434e42827d7bb323bd548d602dbd56
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-08-01 14:00:10 +0200

    Update namespaces.js

--- b4d-blockly/namespaces.js
+++ b4d-blockly/namespaces.js
@@ -7,25 +7,25 @@ This file is being maintaned by Blockly For Discord. Do NOT Modify in the browse
 export class Namespace {
     // E.g. new Namespace('discordjs');
     constructor (name) {
-        this.namespace = name;
+        if (Namespace.blocks[name]) {
+            throw new Error("[Error]: Namespace with name '" + name + "' already exists");
+        } else {
+            this.namespace = name;
+        }
     }
 
     static blocks = {}
 
     // Function to create a block for a namespace
-    createBlock (block_name, data, pre_blockly = undefined) {
-        // if given namespace key hasnt been written into Namespace.blocks yet, put any empty one to avoid errors
+    createBlock (block_name, data, pre_blockly = undefine) {
+        // If given namespace key hasnt been written into Namespace.blocks yet, put any empty one to avoid errors
         if (!Namespace.blocks[this.namespace]) {
             Namespace.blocks[this.namespace] = {};
         }
         if(!Namespace.blocks[this.namespace][block_name]) {
-            // if the block doesnt already exist insert the block info into the proper namespace
-            Namespace.blocks[this.namespace] = {
-                ...Namespace.blocks[this.namespace],
-                [block_name]: { data, pre_blockly }
-            };
-            // NOTE: wtf is this ^
-            
+            // If the block doesnt already exist insert the block info into the proper namespace
+            Namespace.blocks[this.namespace][block_name] = { data, pre_blockly }
+
             console.log(Namespace.blocks);
         } else {
             // Log error if given block already exists.
@@ -34,7 +34,7 @@ export class Namespace {
     }
 
     register(block_name) {
-        // first we get the register_id by combinging the block name and its namespace like so 'namespace:block_name' , and then we also get its data
+        // First we get the register_id by combinging the block name and its namespace like so 'namespace:block_name' , and then we also get its data
         const register_id = this.namespace + ":" + block_name
         const register_data = Namespace.blocks[this.namespace][block_name].data
 
@@ -46,10 +46,10 @@ export class Namespace {
     }
 
     static RegisterFromNamespace(block_name, namespace) {
-        // first two ifs are to check if the namespace, and the block name exist.
+        // Check if the namespace, and the block name exist.
         if (Namespace.blocks[namespace]) {
             if (Namespace.blocks[namespace][block_name]) {
-                // if both conditions are true we register it with blockly by passing the name, and getting the block data from blocks.data (assuming that said data exists)
+                // If both conditions are true we register it with blockly by passing the name, and getting the block data from blocks.data (assuming that said data exists)
                 b4d.Blockly.Blocks[namespace + ':' + block_name] = {
                     init: function() {
                         this.jsonInit(Namespace.blocks[namespace][block_name].data);
@@ -64,7 +64,7 @@ export class Namespace {
     }
 
     static registerAllOf(namespace) {
-        // for loop all of the blocks from the given namespace
+        // For loop all of the blocks from the given namespace
         for (let block in Namespace.blocks[namespace]) {
             Namespace.RegisterFromNamespace(block, namespace);
         }
