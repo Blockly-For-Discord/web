3e9eaf0c336acb9927d6ca094198dc492b629dcd
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-08-01 14:10:28 +0200

    Update channel.js

--- b4d-blockly/libraries/discordjs/blocks/message/channel.js
+++ b4d-blockly/libraries/discordjs/blocks/message/channel.js
@@ -1,15 +1,13 @@
-import { Block } from '/b4d-blockly/index.js';
+import { discordjs } from '/b4d-blockly/libraries/discordjs.js';
 import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
 
-const message_channel = new Block('discordjs', 'message_channel', {
+discordjs.createBlock('message_channel', {
   "message0": "message channel",
   "output": "Channel",
   "colour": b4d.color.channel,
   "tooltip": "Channel of message",
   "helpUrl": ""
 });
-
-message_channel.attach();
 
 b4d.javascriptGenerator.forBlock['discordjs:message_channel'] = function(block, generator) {
   return [`message.channel`, b4d.javascriptGenerator.ORDER_NONE];
