ca38e78ecc920944452f8781114e92f43659c2db
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-08-01 14:08:38 +0200

    Update on_error.js

--- b4d-blockly/libraries/discordjs/blocks/console/on_error.js
+++ b4d-blockly/libraries/discordjs/blocks/console/on_error.js
@@ -1,7 +1,7 @@
-import { Block } from '/b4d-blockly/index.js';
+import { discordjs } from '/b4d-blockly/libraries/discordjs.js';
 import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
 
-const console_on_error = new Block('discordjs', 'console_on_error', {
+discordjs.createBlock('console_on_error', {
   "message0": "When error occurs %1 %2",
   "args0": [
     {
@@ -16,8 +16,6 @@ const console_on_error = new Block('discordjs', 'console_on_error', {
   "tooltip": "When a error occurs run some code (Only use one)",
   "helpUrl": ""
 });
-
-console_on_error.attach();
 
 b4d.javascriptGenerator.forBlock['discordjs:console_on_error'] = function(block, generator) {
   var statements_inner = generator.statementToCode(block, 'inner');
