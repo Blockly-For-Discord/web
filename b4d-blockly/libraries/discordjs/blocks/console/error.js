002f8813fa36c35763b9769c875697b938345e7c
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-08-01 14:08:30 +0200

    Update error.js

--- b4d-blockly/libraries/discordjs/blocks/console/error.js
+++ b4d-blockly/libraries/discordjs/blocks/console/error.js
@@ -1,15 +1,13 @@
-import { Block } from '/b4d-blockly/index.js';
+import { discordjs } from '/b4d-blockly/libraries/discordjs.js';
 import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
 
-const console_error = new Block('discordjs', 'console_error', {
+discordjs.createBlock('console_error', {
   "message0": "error",
   "output": "String",
   "colour": b4d.color.console,
   "tooltip": "The error that occured",
   "helpUrl": ""
 });
-
-console_error.attach();
 
 b4d.javascriptGenerator.forBlock['discordjs:console_error'] = function(block, generator) {
   return [`_ERROR`, b4d.javascriptGenerator.ORDER_NONE];
