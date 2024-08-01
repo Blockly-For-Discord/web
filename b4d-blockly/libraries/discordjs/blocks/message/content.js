0ba30a60efbcfbb503db1abdda42602011cadc5f
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-08-01 14:10:21 +0200

    Update content.js

--- b4d-blockly/libraries/discordjs/blocks/message/content.js
+++ b4d-blockly/libraries/discordjs/blocks/message/content.js
@@ -1,15 +1,13 @@
-import { Block } from '/b4d-blockly/index.js';
+import { discordjs } from '/b4d-blockly/libraries/discordjs.js';
 import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
 
-const message_content = new Block('discordjs', 'message_content', {
+discordjs.createBlock('message_content', {
   "message0": "message content",
   "output": "String",
   "colour": b4d.color.message,
   "tooltip": "Content of message",
   "helpUrl": ""
 });
-
-message_content.attach();
 
 b4d.javascriptGenerator.forBlock['discordjs:message_content'] = function(block, generator) {
   return [`message.content`, b4d.javascriptGenerator.ORDER_NONE];
