5b7faf9ff7aa3116b6cf9782683aad05a02d83ae
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-08-01 14:09:46 +0200

    Update reference.js

--- b4d-blockly/libraries/discordjs/blocks/message/reference.js
+++ b4d-blockly/libraries/discordjs/blocks/message/reference.js
@@ -1,15 +1,13 @@
-import { Block } from '/b4d-blockly/index.js';
+import { discordjs } from '/b4d-blockly/libraries/discordjs.js';
 import * as Blockly from '/components/webpack/blocklycompressed.bundle.js';
 
-const message_reference = new Block('discordjs', 'message_reference', {
+discordjs.createBlock('message_reference', {
   "message0": "message",
   "output": "Message",
   "colour": b4d.color.message,
   "tooltip": "Reference of message",
   "helpUrl": ""
 });
-
-message_reference.attach();
 
 b4d.javascriptGenerator.forBlock['discordjs:message_reference'] = function(block, generator) {
   return [`message`, b4d.javascriptGenerator.ORDER_NONE];
