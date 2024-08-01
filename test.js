a251f76a53a6346876f223fcb8474b8660015017
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-08-01 13:44:01 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 import { Namespace } from '/b4d-blockly/namespaces.js';
 
 // DiscordJS
-import * as DiscordJSBlocks from './b4d-blockly/libraries/discordjs/blocks.js';
-import { DiscordJSTooblox } from './b4d-blockly/libraries/discordjs/index.js';
+import { discordjs } from '/b4d-blockly/libraries/discordjs.js';
+import * as DiscordJSBlocks from '/b4d-blockly/libraries/discordjs/blocks.js';
+import { DiscordJSTooblox } from '/b4d-blockly/libraries/discordjs/index.js';
 
 Namespace.registerAllOf('discordjs');
 
