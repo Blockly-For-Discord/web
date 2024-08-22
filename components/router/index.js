68b592ba0e038c6add305bb4e60b965786813958
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-08-22 20:49:08 +0200

    Update index.js

--- components/router/index.js
+++ components/router/index.js
@@ -6,26 +6,42 @@ Made and Maintained by Blockly For Discord. Any Modification is prohibited and c
 
 import { Router } from './router.js';
 
-import { HomeInit } from '../dashboard/home.js';
-import { HomeSwitch } from '../dashboard/home.js';
+import { HomeInit, HomeSwitch } from '../dashboard/home.js';
 window.HomeSwitch = HomeSwitch;
 window.HomeInit = HomeInit;
-import { Init404 } from '../dashboard/404.js';
-import { Switch404 } from '../dashboard/404.js';
+import { Init404, Switch404 } from '../dashboard/404.js';
 window.Switch404 = Switch404;
 window.Init404 = Init404;
-import { SettingsInit } from '../dashboard/settings.js';
-import { SettingsSwitch } from '../dashboard/settings.js';
+import { SettingsInit, SettingsSwitch } from '../dashboard/settings.js';
 window.SettingsInit = SettingsInit;
 window.SettingsSwitch = SettingsSwitch;
-import { ProjectsInit } from '../dashboard/projects.js';
-import { ProjectsSwitch } from '../dashboard/projects.js';
+import { ProjectsInit, ProjectsSwitch } from '../dashboard/projects.js';
 window.ProjectsInit = ProjectsInit;
 window.ProjectsSwitch = ProjectsSwitch;
+import { ExploreInit, ExploreSwitch } from '../dashboard/explore.js';
+window.ExploreInit = ExploreInit;
+window.ExploreSwitch = ExploreSwitch;
+import { UsersInit, UsersSwitch } from '../dashboard/users.js';
+window.UsersInit = UsersInit;
+window.UsersSwitch = UsersSwitch;
+import { ExtensionsInit, ExtensionsSwitch } from '../dashboard/extensions.js';
+window.ExtensionsInit = ExtensionsInit;
+window.ExtensionsSwitch = ExtensionsSwitch;
+import { UpdatesInit, UpdatesSwitch } from '../dashboard/updates.js';
+window.UpdatesInit = UpdatesInit;
+window.UpdatesSwitch = UpdatesSwitch;
+import { DeveloperInit, DeveloperSwitch } from '../dashboard/developer.js';
+window.DeveloperInit = DeveloperInit;
+window.DeveloperSwitch = DeveloperSwitch;
 
 const Routes = new Router('sidebar-icons', 'sidebar-bottom-region');
 
-Routes.register('home','','components/media/pages/home.svg','top','tooltip',false ,HomeInit, HomeSwitch);
-Routes.register('projects','projects','components/media/pages/projects.svg','top','tooltip',false ,ProjectsInit, ProjectsSwitch);
+Routes.register('home', '', 'components/media/pages/home.svg', 'top', 'tooltip', false, HomeInit, HomeSwitch);
+Routes.register('projects', 'projects', 'components/media/pages/projects.svg', 'top', 'tooltip', false, ProjectsInit, ProjectsSwitch);
+Routes.register('explore', 'explore', 'components/media/pages/explore.svg', 'top', 'tooltip', false, ExploreInit, ExploreSwitch);
+Routes.register('users', 'users', 'components/media/pages/users.svg', 'top', 'tooltip', false, UsersInit, UsersSwitch);
+Routes.register('extensions', 'extensions', 'components/media/pages/extensions.svg', 'top', 'tooltip', false, ExtensionsInit, ExtensionsSwitch);
+Routes.register('updates', 'updates', 'components/media/pages/updates.svg', 'top', 'tooltip', false, UpdatesInit, UpdatesSwitch);
+Routes.register('developer', 'developer', 'components/media/pages/developer.svg', 'top', 'tooltip', false, DeveloperInit, DeveloperSwitch);
 
 Router.loadByPath()
