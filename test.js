a8235d4086e619e6adc1b9714f2adfc904f6a64b
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-08-22 09:09:55 +0000

    Comments and actually use zelos

--- test.js
+++ test.js
@@ -8,16 +8,9 @@ import { DiscordJSTooblox } from '/b4d-blockly/libraries/discordjs/index.js';
 Namespace.registerAllOf('discordjs');
 
 // Workspace & Toolbox
-class CustomRenderer extends b4d.Blockly.zelos.Renderer {
-  constructor() {
-    super();
-  }
-}
-b4d.Blockly.blockRendering.register('custom_renderer', CustomRenderer);
-
 const workspace = b4d.Blockly.inject('blocklyDiv', {
   toolbox: DiscordJSTooblox,
-  renderer: 'custom_renderer',
+  renderer: 'renderer',
   grid: {
     spacing: 30,
     length: 31,
@@ -47,6 +40,7 @@ pattern.setAttribute('height', '27.5');
 pattern.setAttribute('patternTransform', 'scale(3) rotate(125)');
 pattern.innerHTML = `<rect width="100%" height="100%" fill="#888"></rect><path d="M0 10h20z" stroke-width="9" stroke="#ffffff" fill="none"/>`;
 b4d.Blockly.FieldCheckbox.CHECK_CHAR = '\u2714';
+b4d.Blockly.ContextMenuItems.registerCommentOptions();
 
 b4d.toolbox = DiscordJSTooblox;
 b4d.workspace = workspace;
