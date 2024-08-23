f6923e611af6894dd48717cb5b8078cf239e393b
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-08-23 12:31:35 +0200

    Update home.js

--- components/dashboard/home.js
+++ components/dashboard/home.js
@@ -9,8 +9,6 @@ export function HomeInit () {
 export async function HomeSwitch () {
 
     console.log("bye from home")
-    document.getElementById('test').outerHTML = '';
-    const response = `{"response":"success"}`;
     
     return response;
     
