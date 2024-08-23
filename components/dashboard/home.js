51fce1231a3cc5bdb582784942fb096fbe43aa56
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-08-23 11:10:02 +0200

    test

--- components/dashboard/home.js
+++ components/dashboard/home.js
@@ -3,6 +3,9 @@
 export function HomeInit () {
 
     console.log("hello world, from home.")
+    const testElement = document.createElement('sample');
+    testElement.setAttribute('name', 'projects_HelloWorld');
+    document.getElementById('main-area').appendChild(testElement);
 }
 
 
