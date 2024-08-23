56d8ab36e35c5b961ab807d89cb00d4addd60fc0
LarsIsHere <118752107+LarsIsHere@users.noreply.github.com>
2024-08-23 12:07:40 +0200

    stugg

---
+++ components/js/ui.js
@@ -0,0 +1,42 @@
+
+/*
+
+This file is being maintaned by Blockly For Discord. Do NOT Modify in the browser unless you know what you are doing!
+
+*/
+
+let ui = {};
+ui.toggleArea = {};
+ui.selectArea = {};
+ui.mainArea = {};
+
+ui.selectArea.clear = function () {
+    document.getElementById('').innerHTML = '';
+}
+ui.selectArea.hide = function () {
+    document.getElementById('').style.display = 'none';
+}
+ui.selectArea.show = function () {
+    document.getElementById('').style.display = 'block';
+}
+
+ui.mainArea.clear = function () {
+    document.getElementById('').innerHTML = '';
+}
+ui.mainArea.hide = function () {
+    document.getElementById('').style.display = 'none';
+}
+ui.mainArea.show = function () {
+    document.getElementById('').style.display = 'block';
+}
+
+// interactive toggle area
+ui.toggleArea.clear = function () {
+    document.getElementById('').innerHTML = '';
+}
+ui.toggleArea.hide = function () {
+    document.getElementById('').style.display = 'none';
+}
+ui.toggleArea.show = function () {
+    document.getElementById('').style.display = 'block';
+}
