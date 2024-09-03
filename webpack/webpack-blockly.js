3587ae54ff2e25d6c0e909f177fe01a68e1149be
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-09-03 15:53:03 +0000

    fix?

--- webpack/webpack-blockly.js
+++ webpack/webpack-blockly.js
@@ -4,7 +4,7 @@ import * as En from 'blockly/msg/en';
 import {javascriptGenerator} from 'blockly/javascript';
 
 b4d.Blockly = Blockly
-Blockly = Blockly
+window.Blockly = Blockly
 b4d.libraryBlocks = libraryBlocks
 b4d.En = En
 b4d.javascriptGenerator = javascriptGenerator
