3730aa66d4f35ac824794b68114ef257ec86bb5c
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-10-06 23:01:11 +0200

    Fix

--- components/dashboard/projects.js
+++ components/dashboard/projects.js
@@ -1,15 +1,12 @@
-
-
 export function ProjectsInit () {
-
     const testElement = document.createElement('sample');
     testElement.setAttribute('name', 'projects_select-area');
     document.getElementById('select-area').appendChild(testElement);
 }
 
 export async function ProjectsSwitch () {
-
     console.log("bye from projects")
+    document.getElementById('select-area').innerHTML = '';
     const response = `{"response":"success"}`;
     return response;
 }
